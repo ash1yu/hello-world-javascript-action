1141116a850e2fa97cfe87badec0bc0ad4421de6
ashida.yuta <ashida.yuta@jp.panasonic.com>
2024-07-05 12:20:56 +0900

    My first actions

---
+++ index.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    // who-to-greet is the name of the input
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    //get the Json webhook payload for the even that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
